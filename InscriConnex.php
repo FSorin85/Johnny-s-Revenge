714bff1bb5e1418847051da3a2ef53b5f6b1f445
FSorin85 <florian.sorin85@gmail.com>
2018-04-16 18:46:42 +0200

    ajout de la connexion php avec la BDD du formulaire d'inscription

--- InscriConnex.php
+++ InscriConnex.php
@@ -46,7 +46,7 @@ $email = $_POST['mail'];
 $connection = new PDO("mysql:host=localhost;dbname=jr", 'root', '');
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$query = 'INSERT INTO jr.user (id_user, password_user, email_user) VALUES (:pseudo, :password, :email)';
+$query = 'INSERT INTO jr.user (pseudo_user, password_user, email_user) VALUES (:pseudo, :password, :email)';
 $stmt = $connection->prepare($query);
 $stmt->bindValue('pseudo', $pseudo);
 $stmt->bindValue('password', $password);
